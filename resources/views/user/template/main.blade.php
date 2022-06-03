9586d631a5b2bb2cb5a22e72f8459a257fe4c6a2
Firman Anshari Adjie <firmand83@gmail.com>
2022-06-04 05:57:37 +0700

    rename asset

--- resources/views/user/template/main.blade.php
+++ resources/views/user/template/main.blade.php
@@ -16,7 +16,7 @@
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700">
     <!-- Icons -->
     <link rel="stylesheet" href="../../../assets/vendor/nucleo/css/nucleo.css" type="text/css">
-    <link rel="stylesheet" href="../../../assets/vendor/@fortawesome/fontawesome-free/css/all.min.css" type="text/css">
+    <link rel="stylesheet" href="../../../assets/vendor/fortawesome/fontawesome-free/css/all.min.css" type="text/css">
     <!-- Page plugins -->
     <!-- Argon CSS -->
     <link rel="stylesheet" href="../../../assets/css/argon.css?v=1.1.0" type="text/css">
